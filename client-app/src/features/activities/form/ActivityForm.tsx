250e2751731feb164237fcf7a00e5627cf986a21
Flaysen <kamiltuszkiewicz@gmail.com>
2020-06-03 16:42:55 +0200

    Refactored code to use MobX store

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,25 +1,20 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useContext } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import {v4 as uuid } from 'uuid';
+import ActivityStore from '../../../app/stores/activityStore'
+import { observer } from 'mobx-react-lite';
 
 interface IProps {
-    setEditMode: (editMode: boolean) => void;
     activity: IActivity
-    createActivity: (activity: IActivity) => void;
-    editActivity: (activity: IActivity) => void;
-    submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
-    setEditMode,
     activity: initalFormState,
-    createActivity,
-    editActivity,
-    submitting
 
 }) => {
-
+    const activityStore = useContext(ActivityStore)
+    const {createActivity, editActivity, submitting, cancelFormOpen} = activityStore;
     const initializeForm = () => {
     if (initalFormState) {
         return initalFormState
@@ -66,10 +61,10 @@ return (
                 <Form.Input onChange={handleInputChange} name='city' placeholder='City' value={activity.city}/>
                 <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue}/>
                 <Button  loading={submitting} floated='right' positive type='submit' content='Submit'/>
-                <Button onClick={() => setEditMode(false)} floated='right' type='submit' content='Cancel'/>
+                <Button onClick={cancelFormOpen} floated='right' type='submit' content='Cancel'/>
             </Form>
         </Segment>
         )
 }
 
-export default ActivityForm
+export default observer(ActivityForm);
